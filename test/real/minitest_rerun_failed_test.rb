c61d0f9ac2e1e28471587cbbea1a82329d6f1e9d
Søren Houen <houen@users.noreply.github.com>
2026-06-08 12:50:48 +0200

    Fix failed test reruns (#8)

--- test/real/minitest_rerun_failed_test.rb
+++ test/real/minitest_rerun_failed_test.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "fileutils"
+require "open3"
+require "rbconfig"
+require "tmpdir"
 
 # Which number test to run and fail
 FAIL_TEST_NAME = ENV["FAIL_TEST_NAME"]
@@ -28,6 +32,11 @@ else
 end
 
 class MinitestRerunFailedTest < Minitest::Test
+  ROOT = File.expand_path("../..", __dir__)
+  LIB_DIR = File.join(ROOT, "lib")
+  GEMFILE = File.join(ROOT, "Gemfile")
+  CLI_PATH = File.join(ROOT, "bin/rerun_failed_tests")
+
   # Suppress stdout / stderr output from secondary tests.
   # This is because we expect them to print a long stacktrace.
   # With it shown it would always look like tests failed.
@@ -67,6 +76,20 @@ class MinitestRerunFailedTest < Minitest::Test
     File.read("./test_output/#{name}/.minitest_failed_tests.txt")
   end
 
+  def subprocess_env(extra_env = {})
+    {
+      "BUNDLE_GEMFILE" => GEMFILE,
+      "RUBYLIB" => [LIB_DIR, ENV["RUBYLIB"]].compact.join(File::PATH_SEPARATOR)
+    }.merge(extra_env)
+  end
+
+  def capture_ruby(*args, chdir: nil, env: {})
+    options = {}
+    options[:chdir] = chdir if chdir
+
+    Open3.capture3(subprocess_env(env), RbConfig.ruby, *args, **options)
+  end
+
   # Stub test to ensure tests can run.
   # This is here b/c #suppress_output also hides eg Bundler::GemNotFound errors.
   # This test will print and show such issues.
@@ -83,7 +106,7 @@ class MinitestRerunFailedTest < Minitest::Test
   end
 
   def test_it_writes_failed_tests_to_file
-    refute fail_self_file_output.nil?
+    assert_match(%r{\Atest/real/minitest_rerun_failed_test.rb:[0-9]+\n\z}, fail_self_file_output)
   end
 
   def test_it_prints_failed_tests_with_seed
@@ -94,5 +117,101 @@ class MinitestRerunFailedTest < Minitest::Test
   def test_relevant_output_on_unexpected_errors
     assert_match(/Failed tests: [0-9]+ \(seed [0-9]+\)/, fail_self_console_output)
     assert_match(%r{test/real/minitest_rerun_failed_test.rb:[0-9]+\n}, fail_self_console_output)
+  end
+
+  def test_failed_tests_reporter_preserves_failed_exit_status
+    Dir.mktmpdir do |dir|
+      test_file = File.join(dir, "only_failed_reporter_test.rb")
+      File.write(test_file, <<~RUBY)
+        require "minitest/autorun"
+        require "minitest_rerun_failed"
+
+        Minitest::Reporters.use! [
+          Minitest::Reporters::FailedTestsReporter.new(verbose: false, file_output: false)
+        ]
+
+        class OnlyFailedReporterTest < Minitest::Test
+          def test_fail
+            assert false
+          end
+        end
+      RUBY
+
+      _stdout, _stderr, status = capture_ruby(test_file)
+
+      refute status.success?
+    end
+  end
+
+  def test_failed_tests_reporter_honors_file_output_false
+    Dir.mktmpdir do |dir|
+      output_path = File.join(dir, "report")
+      test_file = File.join(dir, "file_output_false_test.rb")
+      File.write(test_file, <<~RUBY)
+        require "minitest/autorun"
+        require "minitest_rerun_failed"
+
+        Minitest::Reporters.use! [
+          Minitest::Reporters::FailedTestsReporter.new(
+            output_path: #{output_path.dump},
+            verbose: false,
+            file_output: false
+          )
+        ]
+
+        class FileOutputFalseTest < Minitest::Test
+          def test_fail
+            assert false
+          end
+        end
+      RUBY
+
+      capture_ruby(test_file)
+
+      refute File.exist?(File.join(output_path, ".minitest_failed_tests.txt"))
+    end
+  end
+
+  def test_rerun_failed_tests_uses_custom_report_dir_and_runs_non_rails_files
+    Dir.mktmpdir do |dir|
+      report_dir = File.join(dir, "report")
+      test_dir = File.join(dir, "space path")
+      FileUtils.mkdir_p([report_dir, test_dir])
+
+      first_test = File.join(test_dir, "first_test.rb")
+      second_test = File.join(test_dir, "second_test.rb")
+      File.write(first_test, "puts \"FIRST_TEST_RAN\"\n")
+      File.write(second_test, "puts \"SECOND_TEST_RAN\"\n")
+      File.write(
+        File.join(report_dir, ".minitest_failed_tests.txt"),
+        "#{first_test}:123\n#{second_test}:456\n"
+      )
+
+      stdout, stderr, status = capture_ruby(
+        CLI_PATH,
+        chdir: dir,
+        env: { "MINITEST_FAILED_TESTS_REPORT_DIR" => "report" }
+      )
+
+      assert status.success?, stderr
+      assert_includes stdout, "FIRST_TEST_RAN"
+      assert_includes stdout, "SECOND_TEST_RAN"
+    end
+  end
+
+  def test_rerun_failed_tests_exits_with_child_status
+    Dir.mktmpdir do |dir|
+      report_dir = File.join(dir, "report")
+      FileUtils.mkdir_p(report_dir)
+      File.write(File.join(report_dir, ".minitest_failed_tests.txt"), "#{File.join(dir, "missing_test.rb")}\n")
+
+      _stdout, _stderr, status = capture_ruby(
+        CLI_PATH,
+        chdir: dir,
+        env: { "MINITEST_FAILED_TESTS_REPORT_DIR" => "report" }
+      )
+
+      refute status.success?
+    end
   end
 end
